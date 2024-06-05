9f9c520ddc6df2dc602ef7d27d9a3e79e3a619a9
KAILANY DE SOUZA MORAES <kailany.smoraes@senacsp.edu.br>
2024-06-05 19:39:54 -0300

    exercicios 1 e 2 feitos (o exercicio 1 foi feito na consulta criacao-tabelas)

--- criacao-tabelas.sql
+++ criacao-tabelas.sql
@@ -5,12 +5,32 @@ CREATE TABLE paciente
 cpf VARCHAR(11) NOT NULL UNIQUE, tipoLogradouro VARCHAR(20) NOT NULL, nomeLogradouro VARCHAR(30) NOT NULL,
 numero CHAR(6) NOT NULL, complemento VARCHAR(20), telefone VARCHAR(11) NOT NULL);
 
-CREATE TABLE consulta 
-(idConsulta INT PRIMARY KEY NOT NULL AUTO_INCREMENT, dataConsulta DATE NOT NULL,
- horaConsulta TIME NOT NULL, tipoConsulta VARCHAR(20) NOT NULL, observacao VARCHAR(30),
- CONSTRAINT fk_idPaciente FOREIGN KEY (idPaciente) REFERENCES paciente(idPaciente),
- CONSTRAINT fk_idDentista FOREIGN KEY (idDentista) REFERENCES dentista(idDentista))
 
-CREATE TABLE Dentista 
-(idDentista INT PRIMARY KEY NOT NULL AUTO_INCREMENT, nomeDentista VARCHAR(50) NOT NULL,
-cro VARCHAR(8) NOT NULL UNIQUE, )
+CREATE TABLE dentista(
+idDentista INT AUTO_INCREMENT NOT NULL,
+nome VARCHAR(150) NOT NULL,
+cro VARCHAR(8) NOT NULL UNIQUE,
+especialidade VARCHAR(100) DEFAULT 'Geral' CHECK (especialidade='Ortodontia' OR 'Geral' OR 
+'Periodontia' OR 'Implantodontia'),
+telefone VARCHAR(11) NOT NULL,
+celular VARCHAR(11) NOT NULL,
+CONSTRAINT PRIMARY KEY(idDentista)
+);
+
+
+CREATE TABLE consulta(
+idConsulta INT AUTO_INCREMENT NOT NULL,
+dataConsulta DATE NOT NULL,
+horaConsulta TIME NOT NULL,
+tipoConsulta VARCHAR(100) NOT NULL,
+idPaciente INT,
+idDentista INT,
+observacao VARCHAR(150),
+CONSTRAINT PRIMARY KEY(idConsulta),
+CONSTRAINT fk_consulta_paciente FOREIGN KEY (idPaciente) REFERENCES paciente(idPaciente),
+CONSTRAINT fk_consulta_dentista FOREIGN KEY (idDentista) REFERENCES dentista(idDentista)
+);
+
+
+
+ALTER TABLE paciente ADD COLUMN cidade VARCHAR(30) NOT NULL 
---
+++ inserção-dados.sql
@@ -0,0 +1,17 @@
+/*querys*/
+
+SELECT * FROM paciente
+
+
+/*exercicio 2: Inserir 6 pacientes de forma implícita, sendo 3 de Santos, 1 de Guarujá e 2 de São Vicente;*/
+INSERT INTO paciente VALUES 
+	('1','Givanna Coutinho', '12665985270', 'rua', 'profa yolanda conte', '220', '', '13996255761', 'São Vicente'),
+	('2','Felipe Andrade', '12876985279','avenida','presidente wilson', '184', 'apartamento 25', '13984255761', 'São Vicente'),
+	('3','Andre da Silva', '45876985268', 'rua', 'dona vitoria', '459', '', '13996080461', 'Guarujá'),
+	('4','José Francisco', '90876385682', 'avenida', 'francisco glicerio', '825', 'apartamneto 60', '13984325780', 'Santos'),
+	('5','Augusto Martins', '18706385682', 'rua', 'alfaia rodrigues', '308', 'casa 2', '13984473580', 'Santos'),
+	('6','Mariana Ribeiro', '34706385682', 'rua', 'alfredo ximenes', '1020', '', '13985673580', 'Santos')
+	
+	
+
+	
